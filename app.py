e9c35340ea70bec21484b0a3f3310a83e4d5c42a
GA-Eduardo <101217685+GA-Eduardo@users.noreply.github.com>
2023-07-17 16:57:58 +0200

    inicio

---
+++ app.py
@@ -0,0 +1,74 @@
+from http import server
+from flask import Flask
+import json
+import os
+import jyserver.Flask as jsf
+from flask import render_template,request
+import time
+from fuzzywuzzy import fuzz
+
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+#load the JSON file
+with open('myjson.json', 'r', encoding='utf-8') as f:
+    chatbot_data = json.load(f)
+   
+
+@jsf.use(app)
+class App:
+   
+    def __init__(self):
+        pass
+
+    def prueba(self):
+      pass    
+    def hola(self):
+        pregunta=str(self.js.dom.search.value)
+        self.js.factor ="Ninguna respuesta obtiene un porcentaje de similitud permitido"
+        user_input=pregunta.lower()
+        mejor_coincidencia=chatbot_data['datos'][0]['pregunta']
+        mejor_respuesta=chatbot_data['datos'][0]['respuesta']  
+        mejor_accion=chatbot_data['datos'][0]['accion'] 
+        mejor_ejecucion=chatbot_data['datos'][0]['path'] 
+        mejor_url=chatbot_data['datos'][0]['url'] 
+         
+       
+        
+        porcentaje_obtenido=0
+                                                  
+        for question in chatbot_data['datos']:
+            porcentaje_iterado=fuzz.token_set_ratio(user_input,question['pregunta'].lower())+\
+            fuzz.token_set_ratio(user_input,question['respuesta'].lower() ) 
+                
+           
+            if porcentaje_iterado>porcentaje_obtenido:
+               
+                mejor_coincidencia=question['pregunta'].lower()
+                mejor_respuesta=question['respuesta'].lower()
+                mejor_accion=question['accion'].lower()
+                mejor_ejecucion=question['path'].lower()
+                mejor_url=question['url'].lower()
+                mejor_seguridad=question['seguridad'].lower()
+                porcentaje_obtenido=porcentaje_iterado
+                if porcentaje_obtenido>80:
+                 self.js.dom.mensaje2.innerHTML="La mejor coincidencia....:  "+str(mejor_coincidencia)
+                 self.js.factor = str(mejor_respuesta) + ". Se ha elegido esta respuesta con una puntuación de "+str(porcentaje_obtenido)
+                 self.js.accion = str(mejor_accion)
+                else:
+                   self.js.dom.mensaje2.innerHTML="Consulta no coincidente"
+                   self.js.factor ="Ninguna respuesta obtiene un porcentaje de similitud permitido"
+          
+                
+      
+      
+          
+
+
+@app.route('/')
+def hello_world():
+  mispreguntas=[]
+  return App.render(render_template('index.html', data=mispreguntas,)) 
+    
+    
+if __name__ == '__main__':
+    app.run('127.0.0.1', 8000, debug=True)    
---
+++ static/js/main.js
@@ -0,0 +1,161 @@
+let factor = '';
+let accion = '';
+window.addEventListener('DOMContentLoaded', (event) => {
+  console.log('DOM fully loaded and parsed');
+  history.pushState(null, null, location.href);
+  history.back();
+  history.forward();
+  window.onpopstate = function () {
+    history.go(1);
+  };
+
+  $('.panel-primero').on('show.bs.collapse', function () {
+    $('#todo').show();
+    $('#search').val('');
+    $('#mensaje').text('Que quieres saber ?');
+    var mipregunta = document.getElementById('search');
+    setTimeout(function () {
+      mipregunta.focus();
+    }, 200);
+  });
+
+  
+});
+
+$('#collapseTwo').on('show.bs.collapse', function (e) {
+  if ($('#mensaje2').text() == 'Te cuento como funciona esta app') {
+    setTimeout(function () {
+      var maq = document.getElementById('leer');
+      var typewriter = new Typewriter(maq, {
+        loop: false,
+      });
+      typewriter
+        .typeString(
+          'Se produce una iteración sobre un archivo Json. Hay una libreria que determina la respuesta más acertada basandose en un algoritmo que ofrece porcentajes de similitud basados en NLP. Procesamiento del lenguaje natural.',
+        )
+        .start();
+    }, 1);
+  }
+});
+
+$('#search').on('change keydown keyup paste input', function (e) {
+  if ($('#search').val() == '') {
+    $('#mensaje').text('Que quieres saber ?');
+  } else {
+    $('#mensaje').text('Ya te estoy escuchando');
+  }
+
+  if (e.which == 13) {
+   
+    if ($('#search').val() == '') {
+      $('#mensaje').text('Debes rellenar una pregunta ?');
+    } else {
+      $('#botonbuscar').click();
+    }
+
+    
+  }
+});
+
+$('#headingTwo').click(function () {
+  $('#botonbuscar').click();
+  setTimeout(function () {
+    $('#search').val('');
+  }, 1000);
+ 
+});
+  
+
+$('#botonbuscar').click(function () {
+  if ($('#search').val() != '') {
+    server.hola();
+    $('#collapseOne').collapse('hide');
+    setTimeout(function () {
+      $('#collapseTwo').collapse('show');
+    }, 1000);
+
+    setTimeout(function () {
+      var maq = document.getElementById('leer');
+      var respuesta = document.getElementById('datos');
+      var typewriter = new Typewriter(maq, {
+        loop: false,
+        delay: 125,
+      });
+      typewriter.typeString(factor).start();
+    }, 1000);
+  }
+   setTimeout(function () {
+     if (accion.includes('ver boton cv')) {
+       $('#micv').show();
+     } else {
+       $('#micv').hide();
+     }
+   }, 1000);
+   setTimeout(function () {
+     if (accion.includes('ver video')) {
+       $('#vervideo').show();
+     } else {
+       $('#vervideo').hide();
+     }
+   }, 1000);
+   setTimeout(function () {
+     if (accion.includes('ver github')) {
+       $('#github').show();
+     } else {
+       $('#github').hide();
+     }
+   }, 1000);
+  
+   setTimeout(function () {
+     if (accion.includes('pdf')) {
+       $('#micvpdf').show();
+     } else {
+       $('#micvpdf').hide();
+     }
+   }, 1000);
+  setTimeout(function () {
+    if (accion.includes('linkedin')) {
+      $('#linkedin').show();
+    } else {
+      $('#linkedin').hide();
+    }
+  }, 1000);
+ 
+});
+
+/*******************************
+ * ACCORDION WITH TOGGLE ICONS
+ *******************************/
+function toggleIcon(e) {
+  $(e.target)
+    .prev('.panel-heading')
+    .find('.more-less')
+    .toggleClass('glyphicon-plus glyphicon-minus');
+}
+$('.panel-group').on('hidden.bs.collapse', toggleIcon);
+$('.panel-group').on('shown.bs.collapse', toggleIcon);
+
+
+$('#todaslaspreguntas').on('change', function () {
+  $('#search').val(this.value)
+  myModal.close();
+   $('#botonbuscar').click();
+});
+
+function iralcv() {
+  window.open('https://cv-eduardo.github.io/', '_blank');
+}
+function iralcvpdf() {
+  window.open('https://cv-eduardo.github.io/docs/Eduardo%20Cabrera%20Bl%C3%A1zquez-cv-2023.pdf', '_blank');
+}
+
+function github() {
+  window.open('https://github.com/Api-Nasa/React-Web', '_blank');
+}
+function vervideo() {
+  window.open('https://mega.nz/file/droGRYiZ#d9t6EW_p2BrXfYGHmxwmeonKKxBX_82JqaF7p9yarbM','_blank');
+}
+
+function verlinkedin() {
+  window.open('https://www.linkedin.com/in/e-cabrera-blazquez/', '_blank');
+}
