f258d4c4ee92cc8db6d28f1ee4d08dea3ae97954
GA-Eduardo <101217685+GA-Eduardo@users.noreply.github.com>
2023-07-28 16:24:15 +0200

    nuevo

--- app.py
+++ app.py
@@ -1,36 +1,31 @@
-from http import server
-from flask import Flask
-import json
-import os
-import jyserver.Flask as jsf
-from flask import render_template,request
-import time
-from fuzzywuzzy import fuzz
 
+##############################################################################
+# sigle page flask "reactivo" interactuando con el Dom sin renderizar de nuevo
+##############################################################################
+
+import jyserver.Flask as js
+from flask import Flask, render_template, request
+import time
+import os
+import json
+from fuzzywuzzy import fuzz
+from rivescript import RiveScript
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 #load the JSON file
 with open('myjson.json', 'r', encoding='utf-8') as f:
     chatbot_data = json.load(f)
-   
+bot=RiveScript()    
+bot.load_file('eduardo.rivescript')
+bot.sort_replies()
 
-@jsf.use(app)
+@js.use(app)
 class App():
-   
-    def __init__(self):
-        pass
-
-    def prueba(self):
-      pass    
-    def hola(self):
-       
-        #self.js.dom.mensaje2.innerHTML="Consulta no coincidente"
-        self.mensaje2="Consulta no coincidente"
-        self.factor="Ninguna respuesta obtiene un porcentaje de similitud permitido"
-        self.acc="Ninguna respuesta obtiene un porcentaje de similitud permitido"
-        #self.js.factor ="Ninguna respuesta obtiene un porcentaje de similitud permitido"
-        pregunta=str(self.js.mipreguntavalor)
-        time.sleep(3)
+    # esta función se puede llamar desde js or html 
+    def cambiar(self,valor):
+         self.js.dom.info1.innerHTML = valor
+    def preguntar(self,pregunta):
+        
         user_input=pregunta.lower()
         mejor_coincidencia=chatbot_data['datos'][0]['pregunta']
         mejor_respuesta=chatbot_data['datos'][0]['respuesta']  
@@ -38,12 +33,13 @@ class App():
         mejor_ejecucion=chatbot_data['datos'][0]['path'] 
         mejor_url=chatbot_data['datos'][0]['url'] 
          
-   
+       
+        
         porcentaje_obtenido=0
                                                   
         for question in chatbot_data['datos']:
-            porcentaje_iterado=fuzz.token_set_ratio(user_input,question['pregunta'].lower())+\
-            fuzz.token_set_ratio(user_input,question['respuesta'].lower() ) 
+            porcentaje_iterado=fuzz.token_sort_ratio(user_input,question['pregunta'].lower())+\
+            fuzz.partial_ratio(user_input,question['respuesta'].lower() ) 
                 
            
             if porcentaje_iterado>porcentaje_obtenido:
@@ -55,36 +51,27 @@ class App():
                 mejor_url=question['url'].lower()
                 mejor_seguridad=question['seguridad'].lower()
                 porcentaje_obtenido=porcentaje_iterado
-                if porcentaje_obtenido>80:
-
+                if porcentaje_obtenido>100:
                  #self.js.dom.mensaje2.innerHTML="La mejor coincidencia....:  "+str(mejor_coincidencia)
-                 #self.js.factor = str(mejor_respuesta) + ". Se ha elegido esta respuesta con una puntuación de "+str(porcentaje_obtenido)
-                 #self.js.accion = str(mejor_accion)
-                 self.mensaje2="La mejor coincidencia....:  "+str(mejor_coincidencia)
-                 self.factor=str(mejor_respuesta) + ". Se ha elegido esta respuesta con una puntuación de "+str(porcentaje_obtenido)
-                 self.accion=str(mejor_accion)
-                 self.acc=str(mejor_accion)
-                
+                 self.js.respuesta = str(mejor_respuesta) + ". Se ha elegido esta respuesta con una puntuación de "+str(porcentaje_obtenido)
+                 self.js.larespuesta(str(mejor_respuesta)+ ". Se ha elegido esta respuesta con una puntuación de "+str(porcentaje_obtenido))
                 else:
                    #self.js.dom.mensaje2.innerHTML="Consulta no coincidente"
-                   #self.js.factor ="Ninguna respuesta obtiene un porcentaje de similitud permitido"
-                   self.mensaje2="Consulta no coincidente"
-                   self.factor="Ninguna respuesta obtiene un porcentaje de similitud permitido"
-        self.js.edu(self.mensaje2,self.factor,self.acc)        
-        time.sleep(3)
-        
-    
-                    
-      
-      
+                   #self.js.respuesta ="Ninguna respuesta obtiene un porcentaje de similitud permitido"
+                   #self.js.larespuesta("Ninguna respuesta obtiene un porcentaje de similitud permitido")  
+                   self.respuesta=bot.reply("localuser",user_input)
+                   self.js.larespuesta(str(self.respuesta))
+         
+                   
+   
           
-
+                 
+                 
+           
+ 
 
 @app.route('/')
-def hello_world():
-  mispreguntas=[]
-  return App.render(render_template('index.html', data=mispreguntas,)) 
-    
-    
-if __name__ == '__main__':
-    app.run('127.0.0.1', 8000, debug=True)    
+def single_page():
+   
+    return App.render(render_template('chat.html'))
+
---
+++ static/js/chat.js
@@ -0,0 +1,100 @@
+let msg=""
+let respuesta="Hola soy Eduardo. Si no sabes que preguntarme, escribe ayuda"
+var $messages = $('.messages-content'),
+    d, h, m,
+    i = 0;
+
+$(window).load(function() {
+  $messages.mCustomScrollbar();
+  setTimeout(function() {
+    fakeMessage();
+  }, 100);
+});
+
+function updateScrollbar() {
+  $messages.mCustomScrollbar("update").mCustomScrollbar('scrollTo', 'bottom', {
+    scrollInertia: 10,
+    timeout: 0
+  });
+}
+
+function setDate(){
+  d = new Date()
+  if (m != d.getMinutes()) {
+    m = d.getMinutes();
+    $('<div class="timestamp">' + d.getHours() + ':' + m + '</div>').appendTo($('.message:last'));
+  }
+}
+
+function insertMessage() {
+  msg = $('.message-input').val();
+  if ($.trim(msg) == '') {
+    return false;
+  }
+  $('<div class="message message-personal">' + msg + '</div>').appendTo($('.mCSB_container')).addClass('new');
+  setDate();
+  $('.message-input').val(null);
+  updateScrollbar();
+  setTimeout(function() {
+    fakeMessage();
+  }, 1000 + (Math.random() * 20) * 100);
+}
+
+$('.message-submit').click(function() {
+  insertMessage();
+});
+
+$(window).on('keydown', function(e) {
+  if (e.which == 13) {
+    insertMessage();
+    return false;
+  }
+})
+
+var Fake = [
+  'Hi there, I\'m Fabio and you?',
+  'Nice to meet you',
+  'How are you?',
+  'Not too bad, thanks',
+  'What do you do?',
+  'That\'s awesome',
+  'Codepen is a nice place to stay',
+  'I think you\'re a nice person',
+  'Why do you think that?',
+  'Can you explain?',
+  'Anyway I\'ve gotta go now',
+  'It was a pleasure chat with you',
+  'Time to make a new codepen',
+  'Bye',
+  ':)'
+]
+
+function fakeMessage() {
+  if ($('.message-input').val() != '') {
+    return false;
+  }
+  if( msg !=""){
+    server.preguntar(msg)
+    
+  }
+  
+  $('<div class="message loading new"><figure class="avatar"><img src="../static/images/foto-chatbot-edu.png" /></figure><span></span></div>').appendTo($('.mCSB_container'));
+  updateScrollbar();
+
+  setTimeout(function() {
+    $('.message.loading').remove();
+    if (respuesta==""){
+      respuesta="error timeout de servidor"
+    }
+    $('<div class="message new"><figure class="avatar"><img src="../static/images/foto-chatbot-edu.png" /></figure>' + respuesta + '</div>').appendTo($('.mCSB_container')).addClass('new');
+    setDate();
+    updateScrollbar();
+    respuesta=""
+    i++;
+  }, 500 + (Math.random() * 20) * 100);
+
+}
+
+function larespuesta(mirespuesta){
+  respuesta=mirespuesta
+}
