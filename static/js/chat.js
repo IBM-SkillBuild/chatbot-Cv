b73d9dd8d4dc412546cb0d2b7f39869771a80912
GA-Eduardo <101217685+GA-Eduardo@users.noreply.github.com>
2023-07-29 13:01:30 +0200

    nuevo33

--- static/js/chat.js
+++ static/js/chat.js
@@ -84,6 +84,7 @@ function escribir(){
     $('.message.loading').remove();
     if (respuesta==""){
       respuesta = "error timeout de servidor"
+      msg=msg2
       server.preguntar()
     }
     $('<div class="message new"><figure class="avatar"><img src="../static/images/foto-chatbot-edu.png" /></figure>' + respuesta + '</div>').appendTo($('.mCSB_container')).addClass('new');
